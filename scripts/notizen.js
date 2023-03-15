cb4b7c18a4c272c187ab6bcffdf5f480e6b97eae
JustinosExcellentus <43820331+JustinosExcellentus@users.noreply.github.com>
2023-03-15 23:54:59 +0100

    Sorting

---
+++ scripts/notizen.js
@@ -0,0 +1,24 @@
+
+var _getAllFilesFromFolder = function(dir) {
+
+    var filesystem = require("fs");
+    var results = [];
+
+    filesystem.readdirSync(dir).forEach(function(file) {
+
+        file = dir+'/'+file;
+        var stat = filesystem.statSync(file);
+
+        if (stat && stat.isDirectory()) {
+            results = results.concat(_getAllFilesFromFolder(file))
+        } else results.push(file);
+
+    });
+
+    return results;
+
+};
+let bodyNotizen = document.getElementById("notizenBody");
+//bodyNotizen.innerHTML += "test";
+console.log(_getAllFilesFromFolder("folder"));
+
