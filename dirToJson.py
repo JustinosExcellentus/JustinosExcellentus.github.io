aa7a6d6f288262f025aa1870fc7a2d0af1f59088
JustinosExcellentus <43820331+JustinosExcellentus@users.noreply.github.com>
2023-03-16 02:19:57 +0100

    test Json

---
+++ dirToJson.py
@@ -0,0 +1,19 @@
+import os
+import json
+
+def path_to_dic(path):
+    d = {'name': os.path.basename(path)}
+    d['path'] = path
+    if os.path.isdir(path):
+        d['typ'] = "directory"
+        d['elemente'] = [path_to_dic(os.path.join(path,sub)) for sub in os.listdir\
+    (path)]
+    else:
+        d['type'] = "file"
+    return d
+
+
+with open('notizStructure.json', 'w') as file:
+    json.dump(json.dumps(path_to_dic('NotizenDir')), file)
+
+
--- scripts/notizen.js
+++ scripts/notizen.js
@@ -5,5 +5,7 @@ var _getAllFilesFromFolder = function(dir) {
 };
 let bodyNotizen = document.getElementById("notizenBody");
 //bodyNotizen.innerHTML += "test";
-console.log(_getAllFilesFromFolder("folder"));
+fetch("notizStructure.json")
+  .then(response => response.json())
+  .then(json => console.log(json));
 
