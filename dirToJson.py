7f36478cbbcdc665e6bc07ea41d7b979338c0fc2
JustinosExcellentus <43820331+JustinosExcellentus@users.noreply.github.com>
2023-03-18 16:23:01 +0100

    commit

--- dirToJson.py
+++ dirToJson.py
@@ -2,10 +2,13 @@ import os
 import json
 
 def path_to_dic(path):
+    
     d = {'name': os.path.basename(path)}
+    print("Working on " + os.path.basename(path))
     d['path'] = path
     if os.path.isdir(path):
         d['typ'] = "directory"
+       
         d['elemente'] = [path_to_dic(os.path.join(path,sub)) for sub in os.listdir\
     (path)]
     else:
