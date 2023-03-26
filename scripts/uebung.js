68b88eb6d3bb30d862aca133fec8273f1c4b8710
JustinosExcellentus <43820331+JustinosExcellentus@users.noreply.github.com>
2023-03-26 17:54:19 +0200

    com

---
+++ scripts/uebung.js
@@ -0,0 +1,31 @@
+let body = document.getElementById("notizenBody");
+
+
+
+
+
+fetch("JSON/uebungStructure.json")
+  .then(response => response.json())
+  .then(json => {
+    function buildList(parent, json) {
+        json.forEach((element) => {
+          let newEntry = document.createElement("li")
+          if(element.typ === "file") {
+            newEntry.classList.add("file");
+            newEntry.innerHTML = `<font size = 3px><a href="${element.path}" download>${element.name}</a></font>`
+            parent.appendChild(newEntry)
+          } else if(element.typ === "directory") {
+            newEntry.classList.add("directory")
+            newEntry.innerHTML = "<font size = 4px>" + element.name + "</font>";
+            parent.appendChild(newEntry)
+            let newParent = document.createElement("ul")
+            newEntry.appendChild(newParent)
+            buildList(newParent, element.elemente);
+          }
+        });
+      }
+
+       console.log(json);
+    
+        buildList(document.body, json.elemente);} );
+
